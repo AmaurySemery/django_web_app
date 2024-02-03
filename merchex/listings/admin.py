e379b8ba5fa661abdc969740b8d0267b6f05209e
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-03 11:41:15 +0100

    Enregistrer un modèle sur le site d'administration et tester les opérations CRUD

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from listings.models import Band
+from listings.models import Band, Listing
 
 class BandAdmin(admin.ModelAdmin):
     list_display = ('name', 'year_formed', 'genre', 'biography') # liste les champs que nous voulons sur l'affichage de la liste
 
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title','description', 'sold', 'year', 'type')
+
 admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
