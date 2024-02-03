ef90f71d81e0b840a78fad1f58f0507e0419b933
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-02-03 23:26:59 +0100

    Lier la vue en liste et la vue détaillée

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -8,5 +8,11 @@ def band_list(request):  # renommer la fonction de vue
         'listings/band_list.html',  # pointe vers le nouveau nom de modèle
         {'bands': bands})
 
+def band_detail(request, id):
+  band = Band.objects.get(id=id)  # nous insérons cette ligne pour obtenir le Band avec cet id
+  return render(request,
+          'listings/band_detail.html',
+          {'band': band}) # nous mettons à jour cette ligne pour passer le groupe au gabarit
+
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
