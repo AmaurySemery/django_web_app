da0a1a1ee29027954559728e1e2fba1c243ff911
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-04-25 16:43:04 +0200

    Supprimer un objet

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -55,6 +55,21 @@ def band_update(request, id):
                 'listings/band_update.html',
                 {'form': form})
 
+def band_delete(request, id):
+    band = Band.objects.get(id=id)  # nécessaire pour GET et pour POST
+
+    if request.method == 'POST':
+        # supprimer le groupe de la base de données
+        band.delete()
+        # rediriger vers la liste des groupes
+        return redirect('band_list')
+
+    # pas besoin de « else » ici. Si c'est une demande GET, continuez simplement
+
+    return render(request,
+                    'listings/band_delete.html',
+                    {'band': band})
+
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
 
